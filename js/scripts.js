772c9e098ed6dd840c8d12dc8f8bda2b5b258bf1
delguanzon <yodelguanzon@gmail.com>
2022-11-04 18:21:31 -0700

    Create function for creating multiple buttons

--- js/scripts.js
+++ js/scripts.js
@@ -68,7 +68,49 @@ function displayTotal() {
   document.getElementById("total-price").replaceChildren(pizza.getPrice());
 }
 
+function displayAllToppings () {
+  let meatToppings = ["Anchovies","Bacon", "Canadian Bacon", "Grilled Chicken", "Ground Beef", "Mild Chicken Sausage", "Pepperoni", "Plant-Based Italian Sausage", "Salami", "Spicy Chicken Sausage"];
+
+  let otherToppings = ["Artichokes", "Arugula", "Basil", "Black", "Olives", "Broccoli-Roasted", "Corn-Roasted", "Garlic-Chopped", "Garlic-Roasted", "Jalapenos","Mushrooms", "Oregano", "Pineapple", "Rosemary", "Salt&Pepper", "Spinach", "Tomatoes-Diced", "Tomatoes-Sliced"];
+
+  let cheeses = ["Asiago", "Feta", "Goronzola", "Mozarella", "Parmesan", "Ricotta", "No Cheese"];
+
+  let finishingSauces = ["BBQ Sauce","Buffalo Sauce", "Ranch", "Blue Cheese", "Balsamic Glaze", "Pesto Drizzle"];
+
+  let meatDiv = document.createElement("div");
+  let cheeseDiv = document.createElement("div");
+  let othersDiv = document.createElement("div");
+  let sauceDiv = document.createElement("div");
+
+  let toppingsDiv = document.getElementById("ordinary-toppings");
+
+  meatToppings.forEach(function (meats) {
+    
+
+  });
+  
+
+  toppingsDiv.append(meatDiv);
+
+
+}
+
+function createButton(name, div){
+  let button = document.createElement("input");
+  let label = document.createElement("label");
+  button.setAttribute("type","checkbox");
+  button.setAttribute("class", "btn-check");
+  button.setAttribute("id", name.split(" ").join.toLowerCase());
+  button.setAttribute("value", name);
+
+  label.setAttribute("class","btn");
+  label.setAttribute("for", name.split(" ").join.toLowerCase());
+  
+}
+
 window.addEventListener("load", function () {
+
+  displayAllToppings();
   document.getElementById("size-section").addEventListener("click", handleSize);
   document.getElementById("toppings-section").addEventListener("click", handleToppings);
 
